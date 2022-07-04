69eb1c742d79900a7186c655fcb9e91e4ba82807
arigato <teeruch.k@gmail.com>
2022-07-04 22:39:44 +0700

    update error message and error code

--- server/pkg/e/msg.go
+++ server/pkg/e/msg.go
@@ -2,10 +2,13 @@ package e
 
 // รับ SUCCESS จาก code.go
 var MessageFlags = map[int]string{
-	SUCCESS:             "ok",
-	ERROR:               "fail",
-	INVALID_PARAMS:      "Invalid parameter",
-	ERROR_ADD_TODO_FAIL: "Can not add todos",
+	SUCCESS:                "ok",
+	ERROR:                  "fail",
+	INVALID_PARAMS:         "Invalid parameter",
+	ERROR_ADD_TODO_FAIL:    "Can not add todos",
+	ERROR_UPDATE_TODO:      "Fail to update todo",
+	ERROR_TODO_NOT_EXIST:   "Todo does not exist",
+	ERROR_DELETE_TODO_FAIL: "Delete todo fail",
 }
 
 func GetMessage(errCode int) string {
