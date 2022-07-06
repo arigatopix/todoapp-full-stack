4385e8fbcd7eb30db01b79ea7b9a99cdf5567fc2
arigato pc wsl <teeruch.k@gmail.com>
2022-07-06 23:36:54 +0700

    mockup todo list

---
+++ frontend/src/app/mock-todo.ts
@@ -0,0 +1,19 @@
+import { Todo } from './interfaces/Todo'
+
+export const todos: Todo[] = [
+  {
+    id: 1,
+    title: "work out",
+    compleated: false,
+  },
+  {
+    id: 2,
+    title: "walk with dog",
+    compleated: true,
+  },
+  {
+    id: 3,
+    title: "some work",
+    compleated: false,
+  },
+]
