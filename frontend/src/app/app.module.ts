fe9556203b00605a3e4ca47235e94f4806c0546d
arigato pc wsl <teeruch.k@gmail.com>
2022-07-06 23:37:24 +0700

    add router link

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { TodolistCardComponent } from './components/todolist-card/todolist-card.
 import { NavComponent } from './components/nav/nav.component';
 import { AddTodoComponent } from './components/add-todo/add-todo.component';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
@@ -15,8 +18,10 @@ import { TodoItemComponent } from './components/todo-item/todo-item.component';
     NavComponent,
     AddTodoComponent,
     TodoItemComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
