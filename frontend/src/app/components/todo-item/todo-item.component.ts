ebba282b0409c8c870f87ed1bb68e5be0354859f
arigato pc wsl <teeruch.k@gmail.com>
2022-07-06 23:37:15 +0700

    add todo item component

--- frontend/src/app/components/todo-item/todo-item.component.ts
+++ frontend/src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import  { Todo } from '../../interfaces/Todo'
+import { todos } from '../../mock-todo'
 
 @Component({
   selector: 'app-todo-item',
@@ -6,6 +9,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-item.component.css'],
 })
 export class TodoItemComponent implements OnInit {
+
+  todos: Todo[] = todos;
+
+  faTimes = faTimes;
   constructor() {}
 
   ngOnInit(): void {}
