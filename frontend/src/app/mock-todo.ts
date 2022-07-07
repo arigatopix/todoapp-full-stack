88ef6a0b0d7ba872f03e9b58d1781b6e93c0a179
arigato pc wsl <teeruch.k@gmail.com>
2022-07-07 20:51:02 +0700

    change interface

--- frontend/src/app/mock-todo.ts
+++ frontend/src/app/mock-todo.ts
@@ -2,17 +2,14 @@ import { Todo } from './interfaces/Todo'
 
 export const todos: Todo[] = [
   {
-    id: 1,
     title: "work out",
     compleated: false,
   },
   {
-    id: 2,
     title: "walk with dog",
     compleated: true,
   },
   {
-    id: 3,
     title: "some work",
     compleated: false,
   },
