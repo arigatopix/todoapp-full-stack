333d992f8317a3c2f95d85d6228655ec2a3afbf9
arigato pc wsl <teeruch.k@gmail.com>
2022-07-09 16:15:55 +0700

    use cookie instead of token headers

--- frontend/src/app/services/todo.service.ts
+++ frontend/src/app/services/todo.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Todo } from '../interfaces/Todo';
 import { Response } from '../interfaces/Response';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -18,7 +18,6 @@ const httpOptions = {
   withCredentials: true,
   headers: new HttpHeaders({
     'Content-Type': 'application/json',
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjYsImV4cCI6MTY1NzI4NDQ3MywiaXNzIjoidG9kb2FwcCJ9.qSNoTsEU_MbVybIg5TYdLxd5pXE1c8jCRDIZL9qc8-0`,
   }),
 };
 @Injectable({
