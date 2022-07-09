a214e35e50e3cc8b67aef77dcc26b344ee29bd31
arigato <teeruch.k@gmail.com>
2022-07-09 22:00:29 +0700

    render email use BehaviorSubject

--- frontend/src/app/components/nav/nav.component.ts
+++ frontend/src/app/components/nav/nav.component.ts
@@ -1,21 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { BehaviorSubject } from 'rxjs';
-
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
-  @Input() email!: string;
-  @Input() signedin!: BehaviorSubject<boolean>
+  emailAuth$: BehaviorSubject<string>;
+  signedin$: BehaviorSubject<boolean>;
 
-  
-  constructor() { 
-   
+  constructor(private authService: AuthService) {
+    this.signedin$ = this.authService.isAuth$;
+    this.emailAuth$ = this.authService.emailAuth$;
+    console.log(this.emailAuth$);
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.authService.getMe().subscribe(() => {});
+  }
 }
