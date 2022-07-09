e6ec54ccf300a385cde37f6658a2e7e306c22407
arigato pc wsl <teeruch.k@gmail.com>
2022-07-09 19:18:09 +0700

    sign up logic

--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerFormGroup! : FormGroup;
 
-  constructor() { }
+  @Input() message: string = ''
+
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    this.registerFormGroup = new FormGroup({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
+      ])
+    })
+  }
+
+  onSignUp(): void {
+    if (this.registerFormGroup.invalid) {
+      this.message = "Please enter your email"
+      return;
+    }
+
+    this.authService.register(this.registerFormGroup.value).subscribe({
+      next: ()=> {
+        this.router.navigateByUrl("/todo")
+      },
+      error: (err) => {
+        this.message = err.error.message
+      }
+    })
+
+    this.registerFormGroup.reset()
+    this.message = ""
   }
 
 }
